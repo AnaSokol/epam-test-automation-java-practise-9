59222507744519ce85ac3d9c711ad60f73a78b87
HvqEMAOU <hvqemaou@email.com>
2021-05-29 21:49:56 +0000

    Update Matrix.java

--- src/main/java/com/epam/test/automation/java/practice9/Matrix.java
+++ src/main/java/com/epam/test/automation/java/practice9/Matrix.java
@@ -7,91 +7,40 @@ public class Matrix {
     private int numberOfRows;
     private int numberOfColumns;
     double[][] matrix;
-
-    /**
-     * Implement a constructor that creates an empty matrix with a given number of rows
-     * columns (all values in matrix equal 0.0)
-     *
-     * @param row    number of rows
-     * @param column number of columns
-     * @return Returns a new instance of the matrix with the specified parameters
-     */
-    public Matrix(int row, int column) {
+    
+    Matrix(int row, int column){
         this.numberOfRows = row;
         this.numberOfColumns = column;
         this.matrix = new double[row][column];
     }
-
-    /**
-     * Implement a constructor that creating of matrix based on existing two-dimensional array.
-     *
-     * @param twoDimensionalArray existing two-dimensional array
-     * @return Returns a new instance of the matrix based on existing two-dimensional array
-     * @throws MatrixException if the incoming array with zero number of rows returns the message "Array passed with zero number of rows",
-     *                         if the incoming array with zero number of columns returns the message "Array passed with zero number of columns"
-     */
-    public Matrix(double[][] twoDimensionalArray) {
+    
+    Matrix(double[][] twoDimensionalArray){
         this.matrix = twoDimensionalArray;
         this.numberOfRows = this.matrix.length;
         this.numberOfColumns = this.matrix[0].length;
     }
 
-    /**
-     * @return Returns the number of rows in a matrix
-     */
     public final int rows() {
         return numberOfRows;
     }
 
-    /**
-     * @return Returns the number of columns in a matrix
-     */
     public final int columns() {
-       return numberOfColumns;
+        return numberOfColumns;
     }
 
-    /**
-     * Receiving of standard two-dimensional array out of matrix.
-     *
-     * @return Standard two-dimensional array
-     */
-    public double[][] twoDimensionalArrayOutOfMatrix() {
-        return matrix;
-    }
-
-    /**
-     * Reading of elements via predetermined correct index
-     *
-     * @param row    number of rows
-     * @param column number of columns
-     * @return Returns the value of a matrix element <code>[row,column]</code>
-     * @throws MatrixException if index incorrect, returns message "Incompatible matrix sizes"
-     */
-    public double getValue(int row, int column) {
+    public double getValue(int row, int column) throws MatrixException {
         double value = matrix[row][column];
         return value;
     }
 
-    /**
-     * Recording value <code>newValue</code> of elements via predetermined correct index <code>[row,column]</code>     *
-     *
-     * @param row      number of rows
-     * @param column   number of columns
-     * @param newValue new value of a matrix element
-     * @throws MatrixException if index incorrect, returns message "Incompatible matrix sizes"
-     */
-    public void setValue(int row, int column, double newValue)  {
+    public void setValue(int row, int column, double newValue) throws MatrixException {
         matrix[row][column] = newValue;
     }
 
-    /**
-     * Method of matrix's addition  <code>matrix</code>.
-     * Result in the original matrix
-     *
-     * @param matrix matrix corresponding to the second term
-     * @return Returns a new resulting matrix
-     * @throws MatrixException if incompatible matrix sizes, returns message "Incompatible matrix sizes"
-     */
+    public double[][] twoDimensionalArrayOutOfMatrix() {
+        return matrix;
+    }
+
     public Matrix addition(Matrix matrix) {
         double[][] temp = new double[numberOfRows][numberOfColumns];
         for(int i=0; i<numberOfRows; i++) {
@@ -103,16 +52,8 @@ public class Matrix {
         return result;
     }
 
-    /**
-     * Method of matrix's deduction <code>matrix</code> from original.
-     * Result in the original matrix
-     *
-     * @param matrix matrix corresponding to the subtracted
-     * @return Returns a new resulting matrix
-     * @throws MatrixException if incompatible matrix sizes, returns message "Incompatible matrix sizes"
-     */
-    public Matrix subtraction(final Matrix matrix)  {
-       double[][] matrixSubstracted = matrix.twoDimensionalArrayOutOfMatrix();
+    public Matrix subtraction(final Matrix matrix) {
+        double[][] matrixSubstracted = matrix.twoDimensionalArrayOutOfMatrix();
         double[][] temp = new double[numberOfRows][numberOfColumns];
         for (int i = 0; i < numberOfRows; i++) {
             for (int j = 0; j < numberOfColumns; j++) {
@@ -123,22 +64,13 @@ public class Matrix {
         return result;
     }
 
-    /**
-     * Method of matrix's multiplication <code>matrix</code>
-     * Result in the original matrix
-     *
-     * @param matrix matrix corresponding to the second factor
-     * @return Returns a new resulting matrix
-     * @throws MatrixException if incompatible matrix sizes, returns message "Incompatible matrix sizes"
-     */
     public Matrix multiplication(final Matrix matrix) {
         double[][] multiplier = matrix.twoDimensionalArrayOutOfMatrix();
         double[][] temp = new double[numberOfRows][matrix.columns()];
         for(int i=0; i<numberOfRows; i++){
-            for(int j=0; j<matrix.columns(); j++){
+            for(int j=0; j<matrix.columns(); j++) {
                 temp[i][j]=0;
-                for(int k=0; k<matrix.rows(); k++)
-                {
+                for(int k=0; k<matrix.rows(); k++) {
                     temp[i][j]+=this.matrix[i][k]*multiplier[k][j];
                 }//end of k loop
             }//end of j loop
@@ -146,6 +78,7 @@ public class Matrix {
         Matrix result = new Matrix(temp);
         return result;
     }
+
 
     @Override
     public String toString() {
